77654b958fd45e7320bd12e339f60b1d26f887e0
KHSavka <42816175+KHSavka@users.noreply.github.com>
2018-12-03 00:45:39 +0200

    getFlags puzzle solved

--- src/getFlags/index.js
+++ src/getFlags/index.js
@@ -6,15 +6,18 @@
  * getFlags(arr); // [1, 2, 3, 4, 5, 6]
  */
 const getFlags = arr => {
-  /* your logic here...*/
-  var sum = []; 
+  /*
+  var flags = [];
 
-  arr.forEach(function(obj) {
-    
-    sum = arr.concat(obj.flags);
-
+  arr.forEach(val => {
+    flags = flags.concat(val.flags);
   });
-return sum;
+
+  return flags;
+  */
+
+  return arr.reduce((acc, val) => acc.concat(val.flags), []);
+  //return arr.reduce((acc, val) => ({flags: acc.flags.concat(val.flags)})).flags;
 };
 
 export default getFlags;
